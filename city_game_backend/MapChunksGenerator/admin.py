1c61c454d1d9ff1408eb46f49e71e52d8eff62f7
wint3rmute <mateusz.baczek1998@gmail.com>
2018-10-14 14:32:16 +0200

    Map chunks generator & data model

--- city_game_backend/MapChunksGenerator/admin.py
+++ city_game_backend/MapChunksGenerator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Chunk, RoadNode
+
+admin.site.register(Chunk)
+admin.site.register(RoadNode)
--- city_game_backend/MapChunksGenerator/models.py
+++ city_game_backend/MapChunksGenerator/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Chunk(models.Model):
+    """
+    A map is divided into chunks - each is 0.01 x 0.01 degrees latitude and longitude,
+    that makes a single chunk around 111x111 meters in size
+    """
+
+    latitude_lower_bound = models.FloatField()
+    longitude_lower_bound = models.FloatField()
+
+    latitude_upper_bound = models.FloatField()
+    longitude_upper_bound = models.FloatField()
+
+
+class RoadNode(models.Model):
+    """
+    A Chunk owns many RoadNodes that store the position of roads inside a given chunk
+    """
+    chunk = models.ForeignKey(Chunk, on_delete=models.CASCADE)
+
+    latitude_start = models.FloatField()
+    latitude_end = models.FloatField()
+
+    longitude_start = models.FloatField()
+    longitude_end = models.FloatField()
