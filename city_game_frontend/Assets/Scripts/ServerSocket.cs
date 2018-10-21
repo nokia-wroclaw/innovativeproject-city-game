b1603d341ec717db6f5197ae58aef11489da4284
kollosp <37954304+kollosp@users.noreply.github.com>
2018-10-21 21:16:23 +0200

    Singleton class, basic data containers classes added

---
+++ city_game_frontend/Assets/Scripts/ServerSocket.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerSocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        int i = Assets.DataManager.instance().getI();
+        Assets.DataManager.instance().setI(20);
+        Debug.Log(i);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
