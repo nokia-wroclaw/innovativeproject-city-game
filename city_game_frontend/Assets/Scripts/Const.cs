2f065f3d62da2eb7f045bac11779decf08c28436
kollosp <pparczyk@wp.pl>
2018-10-22 12:22:04 +0200

    Const variables file added

---
+++ city_game_frontend/Assets/Scripts/Const.cs
@@ -0,0 +1,13 @@
+﻿
+/**
+ * @file contains all const data used in the aplication
+ * 
+ * 
+ * 
+ */
+public static class Const
+{
+    public static string SERVER_IP = "192.168.7.50"; //!< djungo server's ip
+    public static string SERVER_PORT = "2004"; //!< djungo server's port
+}
+
--- city_game_frontend/Assets/Scripts/ServerSocket.cs
+++ city_game_frontend/Assets/Scripts/ServerSocket.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using WebSocketSharp;
+
 public class ServerSocket : MonoBehaviour {
+
+    
 
 	// Use this for initialization
 	void Start ()
@@ -10,10 +14,25 @@ public class ServerSocket : MonoBehaviour {
         int i = Assets.DataManager.instance().getI();
         Assets.DataManager.instance().setI(20);
         Debug.Log(i);
-	}
+
+        using (var ws = new WebSocket(Const.SERVER_IP+":"+Const.SERVER_PORT))
+        {
+            /*ws.OnMessage += (sender, e) =>
+                Console.WriteLine("Laputa says: " + e.Data);
+
+            ws.Connect();
+            ws.Send("BALUS");
+            Console.ReadKey(true);*/
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
         
 	}
+
+    void loadChank()
+    {
+
+    }
 }
