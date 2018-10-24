d4055505188e0486163feded5c8452ec0d345216
Wint3rmute <Mateusz.baczek1998@gmail.com>
2018-10-24 11:43:19 +0200

    where is your god now, git

---
+++ city_game_frontend/Assets/scri.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scri : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("test");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
