c4f7ec71a862b9feef06e8a5b1ad623b74d1cedd
krzysztofnyczka <krzysiek.nyczka@gmail.com>
2018-11-14 17:51:26 +0100

    base for MainMenu development

---
+++ city_game_frontend/Assets/MainMenu/SettingsManagerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsManagerScript : MonoBehaviour {
+
+    public void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+            return;
+        }
+    }
+}
