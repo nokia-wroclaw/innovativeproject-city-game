4bb83c973ebe837c1bfafd0c8de451c39e12e0cf
wint3rmute <mateusz.baczek1998@gmail.com>
2018-12-01 16:18:01 +0100

    customized login data for devs

--- city_game_frontend/Assets/Scripts/RequestData/LoginData.cs
+++ city_game_frontend/Assets/Scripts/RequestData/LoginData.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
 
 [System.Serializable]
 public class LoginData
 {
     public int type = Const.MESSAGE_TYPE_AUTH_EVENT;
-    public string login = "wint3rmute";
-    public string pass = "baczekbezraczek";
+    public string login; //= "wint3rmute";
+    public string pass;//= "baczekbezraczek";
 
     // TODO
     public LoginData()
     {
+        string path = "Assets/Scripts/RequestData/loginData.txt";
+        StreamReader reader = new StreamReader(path);
 
+        try { 
+        string login = reader.ReadLine();
+        string pass = reader.ReadLine();
+
+            this.login = login;
+            this.pass = pass;
+        }
+        catch (System.Exception e) {
+            Debug.LogError("WILL NOT LOG IN UNLESS YOU MAKE A DEFAULT LOGIN DATA FILE");
+        }
     }
 }
