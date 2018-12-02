30a9c0b21f5eceb3f141f35f77a25ef80056b20f
krzysztofnyczka <krzysiek.nyczka@gmail.com>
2018-12-02 18:33:23 +0100

    Added accesible field to manage buildingPanel outputs

--- city_game_frontend/Assets/Scripts/ObjectHandlers/SimpleBuildingPanel.cs
+++ city_game_frontend/Assets/Scripts/ObjectHandlers/SimpleBuildingPanel.cs
@@ -6,9 +6,14 @@ using UnityEngine.UI;
 public class SimpleBuildingPanel : MonoBehaviour {
 
     public GameObject buildingPanel;
-    bool isMouseOver = false;
+    public static bool isOpen = true;
+    public static Sprite img = null;
+    public static string buildingName = null;
 
-    void OnMouseOver()
+    private Text textObj;
+    private Image imgObj;
+
+    /*void OnMouseOver()
     {
         isMouseOver = true;
     }
@@ -16,7 +21,7 @@ public class SimpleBuildingPanel : MonoBehaviour {
     void OnMouseExit()
     {
         isMouseOver = false;
-    }
+    }*/
 
     // Use this for initialization
     void Start () {
@@ -25,11 +30,24 @@ public class SimpleBuildingPanel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (isMouseOver)
+        if (isOpen)
         {
+            if (buildingName != null)
+            {
+                textObj = buildingPanel.GetComponentInChildren<Text>();
+                textObj.text = buildingName;
+            }
+            if(img != null)
+            {
+                imgObj = buildingPanel.GetComponentInChildren<Image>();
+                imgObj.sprite = img;
+            }
+
             buildingPanel.SetActive(true);
             Vector3 panelPos = Camera.main.WorldToScreenPoint(this.transform.position);
+            //panelPos.Set(panelPos.x,panelPos.y+=200, panelPos.z);
             buildingPanel.transform.position = panelPos;
+            //buildingPanel.transform.position.Set(buildingPanel.transform.position.x, buildingPanel.transform.position.y+130, buildingPanel.transform.position.z);
         }
         else
         {
