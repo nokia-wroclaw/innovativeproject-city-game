19554ab89a50a4d309289d06cc2b50b043ba1e4b
wint3rmute <mateusz.baczek1998@gmail.com>
2018-12-17 07:56:39 +0100

    basic backend functions for the Guild model functionality

--- city_game_backend/guild_manager/models.py
+++ city_game_backend/guild_manager/models.py
@@ -9,3 +9,24 @@ class Guild(models.Model):
     Plasmatia = models.FloatField(default=0.0)
     Auferia = models.FloatField(default=0.0)
 
+    def __str__(self):
+        return self.guild_name
+
+    @property
+    def members_count(self):
+        return len(self.player_set.all())
+
+    def add_player(self, player):
+
+        if player.guild is not None:
+            player.guild.remove_player(player)
+
+        player.guild = self
+        player.save()
+
+    def remove_player(self, player):
+        player.guild = None
+        player.save()
+
+        if self.members_count == 0:
+            self.delete()
