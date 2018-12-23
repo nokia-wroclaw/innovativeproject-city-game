cacca260ef17377de586356238f2b6afbd6b5387
wint3rmute <mateusz.baczek1998@gmail.com>
2018-12-23 19:40:08 +0000

    playing around with gtk + python

--- loose_scripts_and_ideas/api_tester/test.py
+++ loose_scripts_and_ideas/api_tester/test.py
@@ -1,16 +1,30 @@
-import PySimpleGUI as sg
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
-# Very basic window.  Return values as a list
+class MyWindow(Gtk.Window):
 
-layout = [
-          [sg.Text('Please enter your Name, Address, Phone')],
-          [sg.Text('Name', size=(15, 1)), sg.InputText('name')],
-          [sg.Text('Address', size=(15, 1)), sg.InputText('address')],
-          [sg.Text('Phone', size=(15, 1)), sg.InputText('phone')],
-          [sg.Submit(), sg.Cancel()]
-         ]
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Hello World")
 
-window = sg.Window('Simple data entry window').Layout(layout)
-button, values = window.Read()
+        self.box = Gtk.Box(spacing=6)
+        self.add(self.box)
 
-print(button, values[0], values[1], values[2])
+        self.button1 = Gtk.Button(label="Hello")
+        self.button1.connect("clicked", self.on_button1_clicked)
+        self.box.pack_start(self.button1, True, True, 0)
+
+        self.button2 = Gtk.Button(label="Goodbye")
+        self.button2.connect("clicked", self.on_button2_clicked)
+        self.box.pack_start(self.button2, True, True, 0)
+
+    def on_button1_clicked(self, widget):
+        print("Hello")
+
+    def on_button2_clicked(self, widget):
+        print("Goodbye")
+
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
