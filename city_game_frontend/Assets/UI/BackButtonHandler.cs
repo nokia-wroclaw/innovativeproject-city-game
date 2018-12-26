f31ada761977b39431af9770436a969f0ed32493
krzysztofnyczka <krzysiek.nyczka@gmail.com>
2018-12-26 14:54:55 +0100

    Added simple guild panel.

--- city_game_frontend/Assets/UI/BackButtonHandler.cs
+++ city_game_frontend/Assets/UI/BackButtonHandler.cs
@@ -3,30 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BackButtonHandler : MonoBehaviour {
+public class BackButtonHandler : MonoBehaviour
+{
 
-    public Animator mainPanel;
+    public Animator mainPanel, guildPanel;
 
     public void ButtonPressed()
     {
-        if(mainPanel.GetBool("isPanelOpen") == true)
+        if (mainPanel.GetBool("isPanelOpen") == true)
         {
             mainPanel.SetBool("isPanelOpen", false);
         }
-        else if(mainPanel.GetBool("isPanelOpen") == false)
+        else if (guildPanel.GetBool("isGuildPanelOpen") == true)
+        {
+            guildPanel.SetBool("isGuildPanelOpen", false);
+        }
+        else if (mainPanel.GetBool("isPanelOpen") == false && guildPanel.GetBool("isGuildPanelOpen") == false)
         {
             SceneManager.LoadScene("MainMenu");
         }
 
     }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
