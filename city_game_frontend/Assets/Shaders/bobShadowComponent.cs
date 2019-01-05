3981e2dbd1b3c21cb43119017714b93fd3db8e09
kollosp <pparczyk@wp.pl>
2019-01-05 18:07:46 +0100

    shadow prefab + setting script

---
+++ city_game_frontend/Assets/Shaders/bobShadowComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/***
+ * Script creates bob shadow using colider size
+ */
+public class bobShadowComponent : MonoBehaviour {
+    public GameObject shadow;
+
+    void Start () {
+        BoxCollider collider = GetComponent<BoxCollider>();
+        
+        Projector projector = shadow.GetComponent<Projector>();
+        shadow.transform.localPosition = new Vector3(0, 0, collider.size.z);
+        shadow.transform.rotation = Quaternion.Euler(90, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
