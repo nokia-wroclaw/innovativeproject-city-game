6125340822c1719801602d8fff1cbd6923efd5c4
krzysztofnyczka <krzysiek.nyczka@gmail.com>
2019-01-19 18:00:01 +0100

    New gui design. To be cd.

---
+++ city_game_frontend/Assets/UI/PanelsContainerHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelsContainerHandler : MonoBehaviour {
+
+    public GameObject container;
+
+    public void showContainer()
+    {
+        container.SetActive(true);
+    }
+
+    public void closeContainer()
+    {
+        container.SetActive(false);
+    }
+
+	// Use this for initialization
+	void Start () {
+        container.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
