1ba8a24846a63f516dba2ad4796f8134eee210a1
krzysztofnyczka <krzysiek.nyczka@gmail.com>
2019-01-20 16:16:35 +0100

    Basic panels logic done.

--- city_game_frontend/Assets/UI/PanelsContainerHandler.cs
+++ city_game_frontend/Assets/UI/PanelsContainerHandler.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class PanelsContainerHandler : MonoBehaviour {
 
     public GameObject container;
+    public GameObject playerPanel,guildPanel,skillTreePanel,actionsPanel,somethingPanel;
+
+    public void closeAllPanelsExcept(GameObject g)
+    {
+        playerPanel.SetActive(false);
+        guildPanel.SetActive(false);
+        skillTreePanel.SetActive(false);
+        actionsPanel.SetActive(false);
+        somethingPanel.SetActive(false);
+
+        g.SetActive(true);
+    }
 
     public void showContainer()
     {
@@ -14,6 +26,37 @@ public class PanelsContainerHandler : MonoBehaviour {
     public void closeContainer()
     {
         container.SetActive(false);
+    }
+
+    public void guildPanelOn()
+    {
+        showContainer();
+        closeAllPanelsExcept(guildPanel);
+        //guildPanel.SetActive(true);
+    }
+
+    public void skillTreePanelOn()
+    {
+        showContainer();
+        closeAllPanelsExcept(skillTreePanel);
+    }
+
+    public void actionsPanelOn()
+    {
+        showContainer();
+        closeAllPanelsExcept(actionsPanel);
+    }
+
+    public void somethingPanelOn()
+    {
+        showContainer();
+        closeAllPanelsExcept(somethingPanel);
+    }
+
+    public void playerPanelOn()
+    {
+        showContainer();
+        closeAllPanelsExcept(playerPanel);
     }
 
 	// Use this for initialization
