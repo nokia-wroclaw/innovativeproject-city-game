6afad20836ee8715f97977a7df05f23ffde155ef
wint3rmute <mateusz.baczek1998@gmail.com>
2019-01-20 13:50:03 +0100

    Reveal scene WIp

---
+++ city_game_frontend/Assets/forwardMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forwardMovement : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y,
+            transform.position.z + speed * Time.deltaTime
+            );
+
+    }
+
+    [ContextMenu("omg")]
+    void loadTrack()
+    {
+
+        for (float i = 0.00F; i < 0.3F; i += 0.001F)
+            MapManager.Instance.sendChunkRequest(17.1F + i, 51.1F);
+
+    }
+}
