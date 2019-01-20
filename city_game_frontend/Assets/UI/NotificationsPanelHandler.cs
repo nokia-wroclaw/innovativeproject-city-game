06e5629753febf6b87f7c8bd81d7e6d84c5c0f61
krzysztofnyczka <krzysiek.nyczka@gmail.com>
2019-01-20 20:19:34 +0100

    Notification panel added.

---
+++ city_game_frontend/Assets/UI/NotificationsPanelHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationsPanelHandler : MonoBehaviour {
+
+    public GameObject notificationPanel;
+    public Text text;
+
+    public void showNotification(string notificationText)
+    {
+        notificationPanel.SetActive(true);
+        text.text = notificationText;
+        Invoke("notificationOn", 3);
+    }
+
+    public void showNotification(string notificationText,int seconds)
+    {
+        notificationPanel.SetActive(true);
+        text.text = notificationText;
+        Invoke("notificationOn", seconds);
+    }
+
+    public void notificationOn()
+    {
+        notificationPanel.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+        notificationPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
