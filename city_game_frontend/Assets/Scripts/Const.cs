7bdae2e776a20f482e144b05701125a93277a40d
krzysztofnyczka <krzysiek.nyczka@gmail.com>
2019-01-25 13:11:51 +0100

    Bar handlers added.

--- city_game_frontend/Assets/Scripts/Const.cs
+++ city_game_frontend/Assets/Scripts/Const.cs
@@ -25,7 +25,8 @@ public static class Const
 	public static int RESOURCE_TYPE_2 = 2;
 	public static int RESOURCE_TYPE_3 = 3;
 	public static int RESOURCE_TYPE_4 = 4;
-	public static string SERVER_URL = "filipplotnicki.com:8000/ws/";
+	public static string SERVER_URL = "156.17.149.214:8000/ws/";
+	//public static string SERVER_URL = "filipplotnicki.com:8000/ws/";
 	public static int SPECIAL_MESSAGE_GUILD_MEMBER_POSITION_UPDATE = 2;
 	public static int SPECIAL_MESSAGE_MAP_UPDATE = 0;
 	public static int SPECIAL_MESSAGE_NOTIFICATION = 1;
