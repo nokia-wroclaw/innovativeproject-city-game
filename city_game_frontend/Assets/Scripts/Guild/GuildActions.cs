970c64f3d2c742ce3ce33c5b71f1a3ceab93b83c
Wint3rmute <Mateusz.baczek1998@gmail.com>
2019-01-28 14:37:02 +0100

    Guild invites backend on Unity side

--- city_game_frontend/Assets/Scripts/Guild/GuildActions.cs
+++ city_game_frontend/Assets/Scripts/Guild/GuildActions.cs
@@ -19,33 +19,76 @@ public class GuildActions : MonoBehaviour {
 
     public Request.callbackFunc guildDataCallback = new Request.callbackFunc((GameObject sender, string error, string data) =>
     {
-        Debug.Log(data);
 
         try { 
-            GuildData retrievedGuildData = JsonUtility.FromJson<GuildData>(data);
-            Debug.Log(retrievedGuildData.name);
-
-            Debug.Log(retrievedGuildData.members[0]);
-            Debug.Log(retrievedGuildData.members[1]);
-
+            GuildDataManager.Instance.guildData = JsonUtility.FromJson<GuildData>(data);
         }
-        catch (System.Exception e)
-        {
+        catch (System.Exception e) {
             Debug.LogError("Cannot parse!");
             Debug.LogError(e);
         }
-         });
+    });
+
+    public void sendCreateGuildRequest(string guildName)
+    {
+        ServerSocket.Instance.send(
+            gameObject,
+            JsonUtility.ToJson(
+                new GuildCreationRequestData(guildName)
+                ), guildCreationCallback);
+    }
+
+    public Request.callbackFunc guildCreationCallback = new Request.callbackFunc((GameObject sender, string error, string data) =>
+    {
+        Debug.Log("Guild created: " + data);
+    });
 
 
     public void inviteToGuild(string nick)
     {
+        ServerSocket.Instance.send(
+            gameObject,
+            JsonUtility.ToJson(
+                new GuildInviteSendRequestData(
+                    nick
+                    )),
+                guildInvitationSendingCallback);
 
     }
 
-    public void kickFromGuild(string nick)
+    public Request.callbackFunc guildInvitationSendingCallback = new Request.callbackFunc((GameObject sender, string error, string data) =>
     {
+        Debug.Log("Invite status: " + data);
+    });
 
+    public void acceptInvite(int invite_id)
+    {
+        answerInvite(Const.GUILD_INVITE_ACCEPT, invite_id);
     }
+
+    public void rejectInvite(int invite_id)
+    {
+        answerInvite(Const.GUILD_INVITE_DENY, invite_id);
+    }
+
+    
+    public void answerInvite(int answer, int invite_id)
+    {
+        ServerSocket.Instance.send(
+            gameObject,
+            JsonUtility.ToJson(
+
+                new GuildInviteResponseRequestData(
+                    answer, invite_id
+                    )
+                ), guildInvitationAnswerCallback);
+    }
+
+    public Request.callbackFunc guildInvitationAnswerCallback = new Request.callbackFunc((GameObject sender, string error, string data) =>
+    {
+        Debug.Log("Invite status: " + data);
+    });
+
 
 
 }
---
+++ city_game_frontend/Assets/Scripts/RequestData/GuildInviteResponseRequestData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GuildInviteResponseRequestData
+{
+    public int type = Const.MESSAGE_TYPE_RESPOND_TO_GUILD_INVITE;
+    public int answer;
+    public int invite_id;
+
+    public GuildInviteResponseRequestData(int answer, int invite_id)
+    {
+        this.answer = answer;
+        this.invite_id = invite_id;
+    }
+}
