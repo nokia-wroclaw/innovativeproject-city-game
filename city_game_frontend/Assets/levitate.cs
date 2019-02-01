71ac9c064b3fee2c56e61fc0dd1b1c87de25bcee
Wint3rmute <Mateusz.baczek1998@gmail.com>
2019-02-01 17:25:37 +0100

    Prefabed 3 more ores

---
+++ city_game_frontend/Assets/levitate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levitate : MonoBehaviour {
+
+    public float speed;
+    public float amplitude;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(0, Mathf.Sin(Time.timeSinceLevelLoad * speed) * amplitude, 0);
+	}
+}
