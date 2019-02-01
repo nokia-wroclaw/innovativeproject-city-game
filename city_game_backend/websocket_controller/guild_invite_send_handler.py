43307b6d0bf8cdf82034334b973d61dff2174d45
wint3rmute <mateusz.baczek1998@gmail.com>
2019-02-01 13:31:04 +0100

    Push notifications for guild invites

--- city_game_backend/websocket_controller/guild_invite_send_handler.py
+++ city_game_backend/websocket_controller/guild_invite_send_handler.py
@@ -3,7 +3,8 @@ from guild_manager.models import GuildInvite
 from player_manager.models import Player
 from city_game_backend import CONSTANTS
 from websocket_controller.WebsocketRoutes import WebsocketRoutes
-
+from websocket_controller.active_connections_storage import ActiveConnectionsStorage
+import json
 
 @WebsocketRoutes.route(CONSTANTS.MESSAGE_TYPE_SEND_GUILD_INVITE)
 @require_message_content(
@@ -24,4 +25,27 @@ def handle_guild_invite_response_request(message, websocket) -> str:
     new_invite.receiver = receiver
     new_invite.save()
 
+    send_invitation_notification(new_invite)
+
     return SUCCESS_MESSAGE
+
+
+def send_invitation_notification(invitation: GuildInvite):
+    receiver_id = invitation.receiver.id
+
+    receivers_websocket = ActiveConnectionsStorage.get(receiver_id)
+
+    if receivers_websocket is None:
+        return
+
+    message: str = json.dumps({
+        'id': CONSTANTS.SPECIAL_MESSAGE_GUILD_INVITE_NOTIFICATION,
+        'message': json.dumps({
+            'guild_name': invitation.guild.guild_name,
+            'invite_id': invitation.id
+        })
+    })
+
+    receivers_websocket.send(
+        message
+    )
