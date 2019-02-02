48e5e5918383c447fe78baef7de9031af06f4326
krzysztofnyczka <krzysiek.nyczka@gmail.com>
2019-02-02 18:53:41 +0100

    Added action buttons.

--- city_game_frontend/Assets/UI/ActionsPanelHandler.cs
+++ city_game_frontend/Assets/UI/ActionsPanelHandler.cs
@@ -6,34 +6,62 @@ public class ActionsPanelHandler : MonoBehaviour {
 
     public GameObject b1, b2, b3;
 
-    public void build1()
+    public GameObject twoButtonsPanel;
+    public GameObject playerButton, plank, OtherButtons;
+
+    public void whenBuildingModeOn()
     {
         PanelsContainerHandler.Instance.closeContainer();
+        twoButtonsPanel.SetActive(true);
+        playerButton.SetActive(false);
+        plank.SetActive(false);
+        OtherButtons.SetActive(false);
+    }
+
+    public void whenBuildingModeOff()
+    {
+        PanelsContainerHandler.Instance.closeContainer();
+        twoButtonsPanel.SetActive(false);
+        playerButton.SetActive(true);
+        plank.SetActive(true);
+        OtherButtons.SetActive(true);
+    }
+
+    public void placeTheBuilding()
+    {
+        placeBuilding.Instance.confirmBuildingPlacement();
+        whenBuildingModeOff();
+    }
+
+    public void exitBuildingMode()
+    {
+        PlayerActions.Instance.leaveBuildingMode();
+        whenBuildingModeOff();
+    }
+
+    public void build1()
+    {
+        whenBuildingModeOn();
         PlayerActions.Instance.enterBuildingMode();
         placeBuilding.Instance.setStructureToBuild(b1, 1);
     }
 
     public void build2()
     {
-        PanelsContainerHandler.Instance.closeContainer();
+        whenBuildingModeOn();
         PlayerActions.Instance.enterBuildingMode();
         placeBuilding.Instance.setStructureToBuild(b2, 2);
     }
 
     public void build3()
     {
-        PanelsContainerHandler.Instance.closeContainer();
+        whenBuildingModeOn();
         PlayerActions.Instance.enterBuildingMode();
         placeBuilding.Instance.setStructureToBuild(b3, 3);
     }
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        twoButtonsPanel.SetActive(false);
 	}
 }
