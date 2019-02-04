6967d8260130b11d188da22d95a115079f3bafe9
wint3rmute <mateusz.baczek1998@gmail.com>
2019-02-04 16:41:20 +0100

    Message ID for chunk owner request

--- city_game_frontend/Assets/Scripts/Const.cs
+++ city_game_frontend/Assets/Scripts/Const.cs
@@ -11,6 +11,7 @@ public static class Const
 	public static int GUILD_INVITE_ACCEPT = 1;
 	public static int GUILD_INVITE_DENY = 2;
 	public static int MESSAGE_TYPE_AUTH_EVENT = 0;
+	public static int MESSAGE_TYPE_CHUNK_OWNER_REQUEST = 13;
 	public static int MESSAGE_TYPE_CHUNK_REQUEST = 2;
 	public static int MESSAGE_TYPE_CREATE_GUILD = 5;
 	public static int MESSAGE_TYPE_DYNAMIC_CHUNK_DATA_REQUEST = 3;
@@ -32,9 +33,8 @@ public static class Const
 	public static int RESOURCE_TYPE_2 = 2;
 	public static int RESOURCE_TYPE_3 = 3;
 	public static int RESOURCE_TYPE_4 = 4;
-    public static string SERVER_URL = "filipplotnicki.com:8000/ws/";
-    //public static string SERVER_URL = "192.168.0.126:8000/ws/";
-    public static int SPECIAL_MESSAGE_GUILD_INVITE_NOTIFICATION = 3;
+	public static string SERVER_URL = "filipplotnicki.com:8000/ws/";
+	public static int SPECIAL_MESSAGE_GUILD_INVITE_NOTIFICATION = 3;
 	public static int SPECIAL_MESSAGE_GUILD_MEMBER_POSITION_UPDATE = 2;
 	public static int SPECIAL_MESSAGE_MAP_UPDATE = 0;
 	public static int SPECIAL_MESSAGE_NOTIFICATION = 1;
