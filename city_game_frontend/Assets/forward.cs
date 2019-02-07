0fe757acb156056be7d4a2bdb0cbb613d28bedd2
wint3rmute <mateusz.baczek1998@gmail.com>
2019-02-07 20:47:59 +0100

    I am become death, the destroyer of worlds

---
+++ city_game_frontend/Assets/forward.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forward : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime);
+	}
+}
