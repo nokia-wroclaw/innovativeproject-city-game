7f55f1bee030e9d2c751014d6e94869434165afd
wint3rmute <mateusz.baczek1998@gmail.com>
2019-03-12 11:37:38 +0100

    Fixing some weird disconnection error

--- city_game_backend/websocket_controller/player_data_request_handler.py
+++ city_game_backend/websocket_controller/player_data_request_handler.py
@@ -13,23 +13,38 @@ def handle_player_data_request(message: dict, websocket) -> str:
     player_guild_invites = GuildInvite.get_invites_of_player(player_to_return)
 
     return_data = {
-        'name': player_to_return.nickname,
-        'level': player_to_return.level,
-        'exp': player_to_return.exp,
-        CONSTANTS.RESOURCE_CEMENTIA: player_to_return.Cementia,
-        CONSTANTS.RESOURCE_PLASMATIA: player_to_return.Plasmatia,
-        CONSTANTS.RESOURCE_AUFERIA: player_to_return.Auferia,
-        'guild': player_to_return.guild.guild_name if player_to_return.guild is not None else None,
+        'name': 'error',
+        'level': 0,
+        'exp': 0,
+        CONSTANTS.RESOURCE_CEMENTIA: 0,
+        CONSTANTS.RESOURCE_PLASMATIA: 0,
+        CONSTANTS.RESOURCE_AUFERIA: 0,
+        'guild': None,
 
-        'invites': [
-            {
-                'guild_name': invite.guild.guild_name,
-                'invite_id': invite.id
-            }
-            for invite in player_guild_invites
-        ]
+        'invites': []
 
     }
+    try:
+        return_data = {
+            'name': player_to_return.nickname,
+            'level': player_to_return.level,
+            'exp': player_to_return.exp,
+            CONSTANTS.RESOURCE_CEMENTIA: player_to_return.Cementia,
+            CONSTANTS.RESOURCE_PLASMATIA: player_to_return.Plasmatia,
+            CONSTANTS.RESOURCE_AUFERIA: player_to_return.Auferia,
+            'guild': player_to_return.guild.guild_name if player_to_return.guild is not None else None,
 
-    print(json.dumps(return_data))
+            'invites': [
+                {
+                    'guild_name': invite.guild.guild_name,
+                    'invite_id': invite.id
+                }
+                for invite in player_guild_invites
+            ]
+
+        }
+    except:
+        pass
+
+    #print(json.dumps(return_data))
     return json.dumps(return_data)
